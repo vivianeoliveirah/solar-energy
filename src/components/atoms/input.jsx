144aee2622ade8fc7ac725e5c9358ca7c5a89958
Neli33 <118769908+Neli33@users.noreply.github.com>
2023-05-05 21:13:08 -0300

    Salvar cadastro

---
+++ src/components/atoms/input.jsx
@@ -0,0 +1,8 @@
+export default function Input({ icon, ...props }) {
+  return (
+    <div className="input-container">
+      {icon && <img src={icon} alt="Ícone" />}
+      <input className={`input ${icon ? "input-with-icon" : ""}`} {...props} />
+    </div>
+  );
+}
--- src/components/organisms/CadastroUnidades.jsx
+++ src/components/organisms/CadastroUnidades.jsx
@@ -1,6 +1,24 @@
+import { useState } from "react";
+
 export default function CadastroUnidades({ setOpenFormulario }) {
+  const [formulario, setFormulario] = useState({
+    apelido: "",
+    local: "",
+    marca: "",
+    modelo: "",
+    ativa: false,
+  });
+  console.log(formulario);
+
   const salvarFormulario = (event) => {
     event.preventDefault();
+
+    fetch("http://localhost:3333/unidades", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formulario),
+    });
+
     setOpenFormulario(false);
   };
   return (
@@ -8,15 +26,75 @@ export default function CadastroUnidades({ setOpenFormulario }) {
       <h2>Cadastro de Unidade Geradora</h2>
       <form onSubmit={salvarFormulario}>
         <label htmlFor="apelido">Apelido</label>
-        <input type="text" name="apelido" id="" />
+        <input
+          type="text"
+          name="apelido"
+          id="apelido"
+          value={formulario.apelido}
+          onChange={(event) => {
+            setFormulario({
+              ...formulario,
+              apelido: event.target.value,
+            });
+          }}
+        />
+
         <label htmlFor="local">Local</label>
-        <input type="text" name="local" id="" />
+        <input
+          type="text"
+          name="local"
+          id="local"
+          value={formulario.local}
+          onChange={(event) => {
+            setFormulario({
+              ...formulario,
+              local: event.target.value,
+            });
+          }}
+        />
+
         <label htmlFor="marca">Marca</label>
-        <input type="text" name="marca" id="" />
+        <input
+          type="text"
+          name="marca"
+          id="marca"
+          value={formulario.marca}
+          onChange={(event) => {
+            setFormulario({
+              ...formulario,
+              marca: event.target.value,
+            });
+          }}
+        />
+
         <label htmlFor="modelo">Modelo</label>
-        <input type="text" name="modelo" id="" />
+        <input
+          type="text"
+          name="modelo"
+          id=""
+          value={formulario.modelo}
+          onChange={(event) => {
+            setFormulario({
+              ...formulario,
+              modelo: event.target.value,
+            });
+          }}
+        />
+
         <label htmlFor="ativo">Ativo</label>
-        <input type="checkbox" name="active" id="" />
+        <input
+          type="checkbox"
+          name="active"
+          id=""
+          checked={formulario.ativa}
+          onChange={(event) => {
+            setFormulario({
+              ...formulario,
+              ativa: event.target.checked,
+            });
+          }}
+        />
+
         <button type="submit">Salvar</button>
       </form>
     </section>
