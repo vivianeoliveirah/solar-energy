206176ff4ccf164f8616f534869d10dc1ff4946b
slsantosjr <slsantosjr@gmail.com>
2023-05-29 19:32:19 -0300

    Update ChartJs

--- src/pages/dashboard/Chart.jsx
+++
@@ -1,6 +0,0 @@
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
-
-ChartJS.register(ArcElement, Tooltip, Legend);
-
-<Doughnut data={...} />
